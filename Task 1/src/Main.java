b7baca6477444fd6cc61c5306dad10119fe016b0
Apeeeq <gasarden@yandex.ru>
2025-03-21 14:17:14 +0300

    Элексилы

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+         boolean isCanCook = false;
 
-        //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Кол-во Жабьие глаза");
+         toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("Кол-во Слезы вурдалака");
+         ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("Кол-во Кости ворона");
+         ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("Кол-во Пельмени");
+         dumplingsCount = new Scanner(System.in).nextInt();
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -13,6 +23,25 @@ public class Main {
         // 3. 7 слез вурдалака + 1 пельмень + 2 жабьих глаза - Эликсир скрытности
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
-        //todo дописать логику программы сюда
+        
+        if(toadEyesCount >= 3 && ravenBonesCount >= 1){
+            System.out.println("Эликсир зоркости");
+            isCanCook = true;
+        }
+        if(ravenBonesCount >= 4 && dumplingsCount >= 4){
+            System.out.println("Элексир стойкости");
+            isCanCook = true;
+        }
+        if(ghoulTearsCount >= 1 && dumplingsCount >= 1 && toadEyesCount >= 2 ){
+            System.out.println("Элексир скрытности");
+            isCanCook = true;
+        }
+        if(ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3){
+            System.out.println("Запретный элексир");
+            isCanCook = true;
+        }
+        if(!isCanCook){
+            System.out.println("Ничего нельзя приготовить");
+        }
     }
 }
